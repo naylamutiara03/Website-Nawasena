1c9972c95f668299813b4f02ead36c911ff4f1fb
Nayla Mutiara Salsabila Bastari <naylamsb03@gmail.com>
2025-01-24 16:09:30 +0700

    page home & services 60%

---
+++ WEB/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      poppins: ['Poppins', 'sans-serif'],
+      opensans: ['Open Sans', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
+
