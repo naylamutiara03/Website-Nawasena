7add2e96f592834512fd965fb2b030dc2de85974
Nayla Mutiara Salsabila Bastari <naylamsb03@gmail.com>
2025-01-31 17:01:40 +0700

    add logo clients & community, edit script.js

--- WEB/components/script.js
+++ WEB/components/script.js
@@ -17,13 +17,15 @@ function renderEvents(events, year) {
       .filter(event => event.year === year)
       .map(event => `
           <div class="bg-gray-800 rounded-lg overflow-hidden shadow-lg">
-              <img class="w-full h-48 object-cover" src="${event.image}" alt="${event.name}">
-              <div class="p-4">
-                  <h3 class="text-xl font-semibold">${event.name}</h3>
-                  <p class="text-gray-400">${event.location}</p>
-                  <a class="mt-2 inline-block bg-cyan-500 text-white px-4 py-2 rounded-full hover:bg-cyan-600 transition" href="#">See More</a>
-              </div>
-          </div>
+            <img class="w-full h-48 object-cover" src="${event.image}" alt="${event.name}">
+            <div class="p-4">
+                <h3 class="text-xl font-semibold">${event.name}</h3>
+                <p class="text-gray-400">${event.location}</p>
+                <div class="flex justify-end"> <!-- Added flex container -->
+                    <a class="mt-2 inline-block bg-cyan-500 text-white px-4 py-2 rounded-full hover:bg-cyan-600 transition" href="#">See More</a>
+                </div>
+            </div>
+        </div>
       `).join('');
 }
 
