73a73faeb0132ca508516dc1b47791c2ed1a4a4e
Nayla Mutiara Salsabila Bastari <naylamsb03@gmail.com>
2025-05-28 10:47:33 +0700

    fade in

--- WEB/tailwind.config.js
+++ WEB/tailwind.config.js
@@ -11,6 +11,15 @@ module.exports = {
         cream: '#FEF9EB',
       },
     },
+    keyframes: {
+      fadeIn: {
+      '0%': { opacity: '0' },
+      '100%': { opacity: '1' },
+      },
+    },
+    animation: {
+      fadeIn: 'fadeIn 2s ease-in-out',
+    },
   },
   plugins: [],
 };
